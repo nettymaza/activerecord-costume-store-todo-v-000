6d587b902b678fa4f690998e30422ea86ff27b8d
Annette Maza <nettymaza@gmail.com>
2017-09-27 17:59:24 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+
+class HauntedHouses< ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
