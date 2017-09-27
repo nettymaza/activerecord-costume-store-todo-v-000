fa82dc8cbf10ce4e6aa4383b97db096ed54b6ad8
Annette Maza <nettymaza@gmail.com>
2017-09-27 18:00:55 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,15 +5,7 @@ class HauntedHouses< ActiveRecord::Migration[4.2]
     create_table :haunted_houses do |t|
       create_table :haunted_houses do |t|
       t.string :name
-      t.string :location
-      t.string :theme
-      t.float :price
-      t.boolean :family_friendly
-      t.datetime :opening_date
-      t.datetime :closing_date
-      t.text :description
-
-      t.timestamps
+    
     end
   end
 end
